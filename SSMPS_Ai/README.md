688b412813da61cc59c1f4972eee82a9d74bcb1a
freenozero <freenozero@naver.com>
2023-05-03 17:27:59 +0900

    update misc_code, md

--- SSMPS_Ai/README.md
+++ SSMPS_Ai/README.md
@@ -10,3 +10,18 @@
 1. face folder: face detection
 2. product folder: product detection
 3. misc_code: image_processing codes
+```
+1. mv: 카테고리 별로 폴더에 들어가 있는 이미지와 라벨링을 한 폴더에 넣기
+(원본 이미지에서 Train Val 나눠진 폴더는 합쳐야 하고, mv를 돌리고 나서 카테고리별 폴더는 삭제)
+
+2. del_not_meta: 일반 xml에는 item_no attribute가 없어서 삭제
+(label_txt 폴더는 직접 만들어야함)
+
+3. xml_to_txt: _meta.xml에서 txt 라벨링 파일로 변경
+
+4. found: image의 라벨링 파일과 txt가 맞게 있는지 확인
+
+5. dataset_split: 카테고리 별로 데이터셋 split
+
+6. create_yaml: data.yaml 파일 만들기(이미지 number로 라벨링)
+```
--- SSMPS_Ai/misc_code/mv.py
+++ SSMPS_Ai/misc_code/mv.py
@@ -1,16 +1,19 @@
 import os
 import shutil
 
-# 이동할 파일이 있는 폴더 경로와 이동할 폴더 경로 설정
-folder_path = r"C:\Users\freet\Documents\GitHub\SSMPS_minju\SSMPS_Ai\product\20_product\label"
+type = ['image', 'label']
+for t in type:
+    # 이동할 파일이 있는 폴더 경로와 이동할 폴더 경로 설정
+    folder_path = r"C:\Users\freet\Documents\GitHub\SSMPS_minju\SSMPS_Ai\product\20_product_2"
+    folder_path = f"{folder_path}/{t}"
 
-folders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]
+    folders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]
 
-for foldername in folders:
-    # 폴더 내 모든 파일에 대해 반복문 실행
-    for filename in os.listdir(f'{folder_path}/{foldername}'):
-        # 이동할 파일 경로 설정
-        src_path = os.path.join(f'{folder_path}/{foldername}', filename)
-        dst_path = os.path.join(folder_path, filename)
-        # 파일을 이동합니다.
-        shutil.move(src_path, dst_path)
+    for foldername in folders:
+        # 폴더 내 모든 파일에 대해 반복문 실행
+        for filename in os.listdir(f'{folder_path}/{foldername}'):
+            # 이동할 파일 경로 설정
+            src_path = os.path.join(f'{folder_path}/{foldername}', filename)
+            dst_path = os.path.join(folder_path, filename)
+            # 파일을 이동합니다.
+            shutil.move(src_path, dst_path)
