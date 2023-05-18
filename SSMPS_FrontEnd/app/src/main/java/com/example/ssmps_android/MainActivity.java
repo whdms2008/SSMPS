b27402c43446eb3a3a1e755c26ecae1b51ed1032
gisu <hks3847@naver.com>
2023-05-18 18:39:18 +0900

    전체 수정

--- SSMPS_FrontEnd/app/src/main/java/com/example/ssmps_android/MainActivity.java
+++ SSMPS_FrontEnd/app/src/main/java/com/example/ssmps_android/MainActivity.java
@@ -31,7 +31,7 @@ import retrofit2.Retrofit;
 public class MainActivity extends AppCompatActivity {
     EditText idInput, passInput;
     TextView guestBtn, joinBtn;
-    Button loginBtn, guestLoginBtn;
+    Button loginBtn;
 
     Retrofit retrofit;
     RetrofitAPI service;
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
     private void initComponent(){
         idInput = findViewById(R.id.login_id);
         passInput = findViewById(R.id.login_password);
-        guestLoginBtn = findViewById(R.id.mainActivity_login_guest);
+        guestBtn = findViewById(R.id.mainActivity_login_guest);
         joinBtn = findViewById(R.id.login_join);
         loginBtn = findViewById(R.id.login_login_btn);
 
@@ -118,10 +118,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void guest_login(){
-        guestLoginBtn.setOnClickListener(new View.OnClickListener() {
+        guestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),FunctionSelectActivity.class);
+                Intent intent = new Intent(getApplicationContext(),GuestActivity.class);
                 startActivity(intent);
             }
         });
