a9e5a051e1828cc883f1baef05185b5158f03733
freenozero <freenozero@naver.com>
2023-05-30 22:13:00 +0900

    삭제 후 총 금액 수정 중

--- SSMPS_PaymentScreen/app/src/main/java/com/example/ssmps_paymentscreen/ListViewAdapter.java
+++ SSMPS_PaymentScreen/app/src/main/java/com/example/ssmps_paymentscreen/ListViewAdapter.java
@@ -1,11 +1,15 @@
 package com.example.ssmps_paymentscreen;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,53 +17,56 @@ import androidx.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListViewAdapter extends ArrayAdapter<ListViewItem> {
+public class ListViewAdapter extends BaseAdapter {
+    private final Context context;
+    private final List<ListViewItem> items;
+
     public ListViewAdapter(Context context, List<ListViewItem> items) {
-        super(context, R.layout.list_item, items);
-        this.listviewItemList.addAll(items);
+        this.context = context;
+        this.items = items;
     }
-
-
-    private final ArrayList<ListViewItem> listviewItemList = new ArrayList<>();
-
 
     @Override
     public int getCount() {
-        return listviewItemList.size();
+        return items.size();
     }
 
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
-        }
-
-        ListViewItem item = getItem(position);
-
-        TextView text1 = convertView.findViewById(R.id.product_name);
-        TextView text2 = convertView.findViewById(R.id.price);
-        TextView text3 = convertView.findViewById(R.id.cnt);
-
-        if (item != null) {
-            text1.setText(item.getText());
-            text2.setText(item.getPrice());
-            text3.setText(item.getCnt());
-        }
-
-        return convertView;
+    @Override
+    public ListViewItem getItem(int position) {
+        return items.get(position);
     }
+
     @Override
     public long getItemId(int position) {
         return position;
     }
 
     @Override
-    public ListViewItem getItem(int position) {
-        return listviewItemList.get(position);
-    }
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+        }
 
-    public void addItem(String text, int price, int cnt) {
-        ListViewItem item = new ListViewItem(text, price, cnt);
-        listviewItemList.add(item);
-    }
+        final ListViewItem item = getItem(position);
 
+        TextView text1 = convertView.findViewById(R.id.product_name);
+        TextView text2 = convertView.findViewById(R.id.price);
+        TextView text3 = convertView.findViewById(R.id.cnt);
+        TextView text4 = convertView.findViewById(R.id.all_price);
+
+        if (item != null) {
+            text1.setText(item.getText());
+            text2.setText(item.getPrice());
+            text3.setText(item.getCnt());
+            text4.setText(item.getAll_price());
+        }
+
+        Button deleteButton = convertView.findViewById(R.id.delButton);
+        deleteButton.setOnClickListener(v -> {
+            Toast.makeText(context, "삭제됨", Toast.LENGTH_SHORT).show();
+            items.remove(position);
+        });
+
+        return convertView;
+    }
 }
